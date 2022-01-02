88338e0d9ecc6e1b00a3a37db09917c900eb0915
waking999 <waking.wang@gmail.com>
2022-01-02 15:55:28 +0930

    AOA v2.5: DT, GBDT, MLP, RF, XGB

---
+++ AOA/B_Process_XGB.py
@@ -0,0 +1,14 @@
+import xgboost as xgb
+from B_Process_Generic import Process
+
+
+class ProcessXGB(Process):
+    def __init__(self, base_file_name, index, model_name):
+        super().__init__(base_file_name, index, model_name)
+        self.model = xgb.XGBRegressor(max_depth=127, learning_rate=0.01, n_estimators=1000,
+                                      objective='reg:tweedie', n_jobs=-1, booster='gbtree')
+
+
+if __name__ == '__main__':
+    process = ProcessXGB(base_file_name='E0a.txt_shuffle.csv', index=0, model_name='xgb')
+    process.process()
